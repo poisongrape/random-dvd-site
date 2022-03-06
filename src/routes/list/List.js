6b9a0ad19d84429a51143ac563fc8a5b3692abfc
poisongrape <10935061+poisongrape@users.noreply.github.com>
2022-03-05 19:43:40 -0800

    Create functions to filter and sort list of dvds.

--- src/routes/list/List.js
+++ src/routes/list/List.js
@@ -10,6 +10,58 @@ const styles = {
   `
 };
 
+/**
+ * Filter dvd items by category.
+ * @todo Move to util file.
+ * @param {object[]} list - Array of dvd items.
+ * @param {string} category - Category to filter on.
+ * @returns {object[]} Array of filtered dvd items.
+ */
+const filterListByCategory = (list, category) => {
+  let filteredList = [...list];
+
+  filteredList = filteredList.filter(item => item.category.toLowerCase() === category.toLowerCase());
+
+  return filteredList;
+}
+
+/**
+ * Sort dvd items alphabetically.
+ * @todo Move to util file.
+ * @param {object[]} list - Array of dvd items.
+ * @returns {object[]} Array of sorted dvd items.
+ */
+const sortListAlphabetically = (list) => {
+  let sortedList = [...list];
+
+  sortedList = sortedList.sort((a, b) => {
+    if (a.name < b.name) { return -1; }
+    if (a.name > b.name) { return 1; }
+    return 0;
+  });
+
+  return sortedList;
+}
+
+/**
+ * Sort dvd items by category first, then by name.
+ * @todo Move to util file.
+ * @param {object[]} list - Array of dvd items.
+ * @returns {object[]} Array of sorted dvd items.
+ */
+const sortListByCategory = (list) => {
+  let sortedList = [...list];
+
+  sortedList = sortListAlphabetically(sortedList);
+  sortedList = sortedList.sort((a, b) => {
+    if (a.category < b.category) { return -1; }
+    if (a.category > b.category) { return 1; }
+    return 0;
+  });
+
+  return sortedList;
+};
+
 export const List = () => {
   const { items } = data;
 
