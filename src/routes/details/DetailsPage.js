9a50991ae13dc987fcaf54554fc4bee1047b4465
poisongrape <10935061+poisongrape@users.noreply.github.com>
2022-03-05 18:22:23 -0800

    Add detail page to show dvd image and title.

--- src/routes/details/DetailsPage.js
+++ src/routes/details/DetailsPage.js
@@ -1,5 +1,36 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { css } from "@emotion/react"
 
+import { Dvd } from "../home/Dvd";
+import data from "../../data/dvd-data.json";
+
+const styles = {
+  dvdContainer: css`
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
+}
 export const DetailsPage = () => {
-  return <h1>Details Page</h1>
+  const params = useParams();
+  const { dvdId } = params;
+  const { items } = data;
+
+  let dvdIdNum = parseInt(dvdId, 10);
+  const dvdData = items.find(item => item.id === dvdIdNum);
+
+  if (!dvdData) {
+    return <Typography variant="h1">Could not find DVD</Typography>;
+  }
+
+  return (
+    <section>
+      <Box sx={styles.dvdContainer}>
+        <Dvd id={dvdData.id} name={dvdData.name} image={dvdData.image} />
+      </Box>
+    </section>
+  )
 };
